6b2b31d70ef5a906a049d6a34e68dc1e996fb0ae
rgllm <a74634@alunos.uminho.pt>
2016-03-08 18:22:36 +0000

    carregar vendas a funcionar

--- project/main.c
+++ project/main.c
@@ -1,104 +1,51 @@
 #include <stdio.h>
 #include <string.h>
-#include "avlLib/avl.h"
+#include <stdlib.h>
+#define MAXBUFF 64
 
-#define MAXBUFF 128
+void main(){
 
-char aclientes[20000][10], aprodutos[200000][10];
+	int c=0,j,i,nc,np;
+	char buffer[MAXBUFF], clientes[20000][10], produtos[200000][10];
+	FILE *fp;
 
-int nclientes(){
-
-char buffer[MAXBUFF];
-int n=0;
-FILE *fp=fopen("files/Clientes.txt","r");
-
-while( fgets (buffer, MAXBUFF, fp)){
-		strcpy(aclientes[n],buffer);
-		strtok(aclientes[n],"\r\n");
-	   n++;
-	}
-
-fclose(fp);
-return n;
-}
-
-int nprodutos(){
-
-char buffer[MAXBUFF];
-int n=0;
-FILE *fp=fopen("files/Produtos.txt","r");
-
-while( fgets (buffer, MAXBUFF, fp)){
-	strcpy(aprodutos[n],buffer);
-	strtok(aprodutos[n],"\r\n");
-	n++;
-}
-fclose(fp);
-return n;
-}
-
-
-
-int nvendas(int clientes, int produtos){
-
-int n=0,i,j;
-char buffer[MAXBUFF];
-char* aux;
-char *s1,*s2;
-FILE *fp=fopen("files/Vendas1.txt","r");
-
-while( fgets(buffer, MAXBUFF, fp)){
-	s1=strtok(buffer," ");
-	strtok(NULL," ");
-	strtok(NULL," ");
-	strtok(NULL," ");
-	s2=strtok(NULL," ");
-for(i = 0; i < clientes; i++){
-	if(!strcmp(aclientes[i],s2)){
-		for(j = 0; j <produtos; j++){
-			if(!strcmp(aprodutos[i],s1)) n++;
-		}
-	}
-}
-}
-
-fclose(fp);
-return n;
-}
-
-
-void carregaClientes(struct avl_table * clientes[]){
-	int i=0;
-	char aux[10];
-	char buffer[MAXBUFF];
-	FILE *fp=fopen("files/Clientes.txt","r");
-
-	for(i=0;i<26;i++){
-		clientes[i]=avl_create(strcmp,NULL,&avl_allocator_default);
-	}
-	while( fgets (buffer, MAXBUFF, fp)){
-			strcpy(aux,buffer);
-			strtok(aux,"\r\n");
-		   	avl_t_insert (clientes[(aux[0]-65)], aux);
-		}
-
+//CLIENTES
+	fp = fopen( "files/Clientes.txt", "r" );
+	while (fgets(buffer, MAXBUFF,fp)!=NULL) {strcpy(clientes[c],buffer);strtok(clientes[c],"\r\n");c++;}
+	printf("%d\n", c);
 	fclose(fp);
+	nc=c;
 
-}
+//PRODUTOS
+	c=0;
+	fp = fopen( "files/Produtos.txt", "r" );
+	while (fgets(buffer, MAXBUFF,fp)!=NULL) {strcpy(produtos[c],buffer);strtok(produtos[c],"\r\n");c++;}
 
+	printf("%d\n", c);
+	fclose(fp);
+	np=c;
 
-int main(){
-int clientes,produtos,vendas;
+//VENDAS
+	c=0;i=0;
+	char* s1, *s2;
+	fp = fopen( "files/Vendas1.txt", "r" );
+	while (fgets(buffer, MAXBUFF,fp)!=NULL){
+			s1=strtok(buffer," ");
+			strtok(NULL," ");
+			strtok(NULL," ");
+			strtok(NULL," ");
+			s2=strtok(NULL," ");
 
-struct avl_table aClientes[26];
-carregaClientes(&aClientes);
+			for(i = 0; i < nc; i++)
 
-clientes=nclientes();
-printf("Número de Clientes : %d\n",clientes );
-produtos=nprodutos();
-printf("Número de Produtos : %d\n",produtos );
-vendas=nvendas(clientes, produtos);
-printf("Número de Vendas : %d\n",clientes );
+		   		 if(strcmp(clientes[i],s2)==0)
 
-return 1;
+		     			for(j = 0; j < np; j++)
+
+			 		  		if(strcmp(produtos[j],s1)==0) {c++; break;}
+
+		    }
+
+    printf("%d\n", c);
+	fclose(fp);
 }

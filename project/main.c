cfde93eb2d3d4f271890940eced7537b80e2a990
munybt <samuel_refong@hotmail.com>
2016-03-31 17:49:26 +0100

    escrita das vendas validas num ficheiro

--- project/main.c
+++ project/main.c
@@ -1,17 +1,74 @@
 #include "clientes.h"
 #include "produtos.h"
-#include "vendas.h"
 #define MAXBUFF 64
+
+typedef nodoV* venda;
+typedef venda catVendas[26];
+
+#ifndef INFO
+#define INFO
+typedef struct info{
+    char produto[8];
+    double preco;
+    int qtd;
+    char np;
+    char cliente[8];
+    int mes;
+    int filial;
+}info;
+#endif
+
+void registaVenda(info * inf,char produto[], double preco, int qtd, char np, char cliente[], int mes, int filial){
+    strcpy(inf->produto,produto);
+    inf->preco = preco;
+    inf->qtd = qtd;
+    inf->np = np;
+    strcpy(inf->cliente,cliente);
+    inf->mes = mes;
+    inf->filial = filial;
+}
+
+venda criaVenda(info inf){
+    return criaNodoV(inf,NULL);
+}
+
+venda insereVenda(info cod,venda raiz){
+    return insertV(cod,raiz);
+}
+
+void initV(venda * c){
+    int i;
+    for(i=0;i<26;i++)
+        c[i]=NULL;
+}
+
+int validaVenda(catClientes clientes,catProdutos produtos,info* inf){
+	int aux;
+	aux=inf->produto[0]-65;
+	if(search(produtos[aux],inf->produto)!=NULL){
+		aux=inf->cliente[0]-65;
+		if(search(clientes[aux],inf->cliente)!=NULL){
+			if(inf->qtd>0 && 
+				inf->preco>=0 &&
+				inf->mes>=1 && inf->mes <=12 &&
+				(inf->np=='N' || inf->np=='P') &&
+				inf->filial>=1 && inf->filial<=3 )
+				return 0;
+		}
+	}
+	return 1;
+}
+
 
 int main(){
 	catClientes clientes;
 	catProdutos produtos;
 	catVendas vendas;
 	int aux,c=0,qtd,mes,fil;
-	char cod[10], buffer[MAXBUFF],*prod,np,*cli,*precAux;
+	char cod[10],linha[MAXBUFF], buffer[MAXBUFF],*prod,np,*cli,*precAux;
 	double prec;
 	info inf;
-	FILE *fp;
+	FILE *fp,*fp2;
 
 	/*                 Leitura dos clientes                 */
 	
@@ -60,33 +117,25 @@ int main(){
 	c=0;
 	initV(vendas);
     fp = fopen( "files/Vendas1.txt", "r" );
+    fp2=fopen("Vendas_1MValidas.txt", "w");
 	while (fgets(buffer, MAXBUFF,fp)!=NULL){
+		strcpy(linha,buffer);
 		prod=strtok(buffer," ");
-		aux=prod[0]-65;
-		if(search(produtos[aux],prod)!=NULL){
-			prec=strtod(strtok(NULL," "),&precAux);
-			qtd=atoi(strtok(NULL," "));
-			np=strtok(NULL," ")[0];
-			cli=strtok(NULL," ");
-			aux=cli[0]-65;
-			if(search(clientes[aux],cli)!=NULL){
-				mes=atoi(strtok(NULL," "));
-				fil=atoi(strtok(NULL," "));
-				registaVenda(&inf,prod,prec,qtd,np,cli,mes,fil);
-				aux=prod[0]-65;
-				if(vendas[aux]==NULL){
-					vendas[aux]=criaVenda(inf);
-					c++;
-				}
-				else if (searchV(vendas[aux],inf)==NULL){
-						vendas[aux]=insereVenda(inf,vendas[aux]);
-						c++;
-					 }
-			}
+		prec=strtod(strtok(NULL," "),&precAux);
+		qtd=atoi(strtok(NULL," "));
+		np=strtok(NULL," ")[0];
+		cli=strtok(NULL," ");
+		mes=atoi(strtok(NULL," "));
+		fil=atoi(strtok(NULL," "));
+		registaVenda(&inf,prod,prec,qtd,np,cli,mes,fil);
+		if(validaVenda(clientes,produtos,&inf)==0){
+			fprintf(fp2,"%s", linha);
+			c++;
 		}
 	}
+	printf("Vendas:%d\n",c );
+	fclose(fp2);
 	fclose(fp);
-	printf("Vendas: %d\n", c);
 
 	return 1;
 }

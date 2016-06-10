7234527f311eba0de5203f6e0e0affb725d169c1
rgllm <a74634@alunos.uminho.pt>
2016-06-10 11:15:47 +0100

    menus e outras coisas

--- GereVenda/src/GereVendaApp.java
+++ GereVenda/src/GereVendaApp.java
@@ -1,22 +1,82 @@
 
 import java.io.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author rgllm
- */
 public class GereVendaApp {
     
     private static Hipermercado hip = new Hipermercado();
+    private static Menu menuPrincipal;
     
+    
+    private static void carregarMenus() {
+        String[] principal={"Ler os ficheiros com os dados"};
+        menuPrincipal = new Menu(principal);
+    }
+    
+   public final static void clearScreen(){
+       
+    }
+
+    private static void leituraFicheiros(){
+        String ficheiroVendas;
+        try{
+            clearScreen();
+            System.out.print("\nInsira o nome do ficheiro de vendas: ");
+            ficheiroVendas=Input.lerString();
+            Crono.start();
+            hip.setCatalogoClientes(Leitura.leituraClientes());
+            Crono.stop();
+            System.out.println("Tempo leitura clientes: "+Crono.print());
+            Crono.start();
+            hip.setCatalogoProdutos(Leitura.leituraProdutos());
+            Crono.stop();
+            System.out.println("Tempo leitura Produtos: "+Crono.print());
+            Crono.start();
+            hip.leituraVendas(ficheiroVendas);
+            Crono.stop();
+            System.out.println("Tempo leitura Vendas: "+Crono.print());
+        }
+       catch(NullPointerException e){
+            System.out.println("Um dos ficheiros não foi encontrado.\n");
+        } 
+    }
+    
+    public final static void printLogo(){
+    }
  
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException{
+        carregarMenus();
+        //initApp();
+        do{
+           menuPrincipal.executa();
+           switch(menuPrincipal.getOpcao()){
+               case 1:
+                   clearScreen();
+                   leituraFicheiros();    
+           }
+        } while (menuPrincipal.getOpcao()!=0);
+        /*try{
+           hip.gravarEstado("GereVenda.data");
+        }
+        catch(IOException e){
+            System.out.println("Não consegui gravar os dados.");
+        } */
+        System.out.println("Até à próxima!");
+   }
+
+}  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+   }
         
         try{
             Crono.start();
@@ -25,16 +85,16 @@ public class GereVendaApp {
             System.out.println("Tempo leitura clientes: "+Crono.print());
             /*System.out.println("Clientes lidos: "+catalogoClientes.size());*/
             
-            Crono.start();
+           /* Crono.start();
             hip.setCatalogoProdutos(Leitura.leituraProdutos());
             Crono.stop();
-            System.out.println("Tempo leitura Produtos: "+Crono.print());
+            System.out.println("Tempo leitura Produtos: "+Crono.print()); */
             /*System.out.println("Produtos lidos: "+catalogoProdutos.size()); */
             
-            Crono.start();
+            /*Crono.start();
             hip.leituraVendas("Vendas_1M.txt");
             Crono.stop();
-            System.out.println("Tempo leitura Vendas: "+Crono.print());
+            System.out.println("Tempo leitura Vendas: "+Crono.print()); */
             
             /*
             Crono.start();
@@ -46,18 +106,13 @@ public class GereVendaApp {
             hip.gravarEstado("hipermercado.1m");
             */
             
-        }
+        /*}
         catch(NullPointerException e){
             System.out.println("Ficheiro não encontrado.\n");
-        }
+        } */
         /*
         catch( IOException | ClassNotFoundException e){
             System.out.println("Erro na escrita/leitura do ficheiro.\n");
             e.printStackTrace();
         }
         */
-
-    }
-    
-    
-}
--- GereVenda/src/Hipermercado.java
+++ GereVenda/src/Hipermercado.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 import java.io.*;
 import java.util.*;
@@ -133,7 +127,7 @@ public class Hipermercado implements java.io.Serializable {
             }
         }
         catch(NullPointerException e){
-            System.out.println("You have to have a file.\n");
+            System.out.println("Erro ao ler o ficheiro.\n");
         }
     }
 }

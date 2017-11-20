a323a77f7a976582d8d8c0232a42d14ec01d7ea2
Rogério Moreira <r@rgllm.com>
2017-11-20 11:05:15 +0000

    file reader centralestacoes

--- src/SuperBike/src/centralestacoes/CentralEstacoes.java
+++ src/SuperBike/src/centralestacoes/CentralEstacoes.java
@@ -1,27 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package centralestacoes;
 
-/**
- *
- * @author rgllm
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /*
-*
+* Ir buscar estações a um ficheiro
+* Carregar para a memória
+* Recebe um listar
+* envia a lista de estações
 */
 
 
 public class CentralEstacoes {
+    
+    public static ArrayList<String> estacoes;
+    private static final String FILENAME = "estacoes.txt";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        estacoes= new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+		estacoes.add(sCurrentLine);
+            }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }

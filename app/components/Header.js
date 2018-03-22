7d672df6594e0579b37cd851bf4104bb28dd96a6
Rogério Moreira <r@rgllm.com>
2018-03-22 15:22:14 +0000

    fixing heroku 🤦

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,37 +1,9 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
-import { connect } from 'react-redux'
-import { logout } from '../actions/auth';
 
 class Header extends React.Component {
-  handleLogout(event) {
-    event.preventDefault();
-    this.props.dispatch(logout());
-  }
-
   render() {
     const active = { borderBottomColor: '#3f51b5' };
-    const rightNav = this.props.token ? (
-      <ul className="nav navbar-nav navbar-right">
-        <li className="dropdown">
-          <a href="#" data-toggle="dropdown" className="navbar-avatar dropdown-toggle">
-            <img src={this.props.user.picture || this.props.user.gravatar}/>
-            {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
-            <i className="caret"></i>
-          </a>
-          <ul className="dropdown-menu">
-            <li><Link to="/account">My Account</Link></li>
-            <li className="divider"></li>
-            <li><a href="#" onClick={this.handleLogout.bind(this)}>Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    ) : (
-      <ul className="nav navbar-nav navbar-right">
-        <li><Link to="/login" activeStyle={active}>Log in</Link></li>
-        <li><Link to="/signup" activeStyle={active}>Sign up</Link></li>
-      </ul>
-    );
     return (
       <nav className="navbar navbar-default navbar-static-top">
         <div className="container">
@@ -49,7 +21,6 @@ class Header extends React.Component {
               <li><IndexLink to="/" activeStyle={active}>Home</IndexLink></li>
               <li><Link to="/contact" activeStyle={active}>Contact</Link></li>
             </ul>
-            {rightNav}
           </div>
         </div>
       </nav>
@@ -57,11 +28,4 @@ class Header extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    token: state.auth.token,
-    user: state.auth.user
-  };
-};
-
-export default connect(mapStateToProps)(Header);
+export default Header;
--- server.js
+++ server.js
@@ -10,10 +10,6 @@ var React = require('react');
 var ReactDOM = require('react-dom/server');
 var Router = require('react-router');
 var Provider = require('react-redux').Provider;
-var mongoose = require('mongoose');
-var jwt = require('jsonwebtoken');
-var moment = require('moment');
-var request = require('request');
 
 // Load environment variables from .env file
 dotenv.load();
@@ -22,11 +18,7 @@ dotenv.load();
 require('babel-core/register');
 require('babel-polyfill');
 
-// Models
-var User = require('./models/User');
-
 // Controllers
-var userController = require('./controllers/user');
 var contactController = require('./controllers/contact');
 
 // React and Server-Side Rendering
@@ -35,12 +27,6 @@ var configureStore = require('./app/store/configureStore').default;
 
 var app = express();
 
-
-mongoose.connect(process.env.MONGODB);
-mongoose.connection.on('error', function() {
-  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
-  process.exit(1);
-});
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
@@ -52,46 +38,11 @@ app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  req.isAuthenticated = function() {
-    var token = (req.headers.authorization && req.headers.authorization.split(' ')[1]) || req.cookies.token;
-    try {
-      return jwt.verify(token, process.env.TOKEN_SECRET);
-    } catch (err) {
-      return false;
-    }
-  };
-
-  if (req.isAuthenticated()) {
-    var payload = req.isAuthenticated();
-    User.findById(payload.sub, function(err, user) {
-      req.user = user;
-      next();
-    });
-  } else {
-    next();
-  }
-});
-
 app.post('/contact', contactController.contactPost);
-app.put('/account', userController.ensureAuthenticated, userController.accountPut);
-app.delete('/account', userController.ensureAuthenticated, userController.accountDelete);
-app.post('/signup', userController.signupPost);
-app.post('/login', userController.loginPost);
-app.post('/forgot', userController.forgotPost);
-app.post('/reset/:token', userController.resetPost);
-app.get('/unlink/:provider', userController.ensureAuthenticated, userController.unlink);
-app.post('/auth/facebook', userController.authFacebook);
-app.get('/auth/facebook/callback', userController.authFacebookCallback);
-app.post('/auth/google', userController.authGoogle);
-app.get('/auth/google/callback', userController.authGoogleCallback);
-app.post('/auth/twitter', userController.authTwitter);
-app.get('/auth/twitter/callback', userController.authTwitterCallback);
 
 // React server rendering
 app.use(function(req, res) {
   var initialState = {
-    auth: { token: req.cookies.token, user: req.user },
     messages: {}
   };
 

83ee9334b4f0f7adfc1986c8404ca476d3dd6b44
MikeDM16 <miguel_miranda96@live.com.pt>
2018-05-02 18:42:00 +0100

    tentativa de codificação inicial

--- src/ga/lrp.R
+++ src/ga/lrp.R
@@ -5,28 +5,34 @@ library(jsonlite)
 
 json <- fromJSON("../Desktop/cn3/data/json/o.json")
 
-n_customers = json$meta_data$nb_customers
-n_depots = json$meta_data$nb_depots
+nr_clientes = json$meta_data$nb_customers
+nr_depositos = json$meta_data$nb_depots
 vehicle_cap = json$meta_data$vehicle_cap
 vehicle_cost = json$meta_data$vehicle_cost
 cost_type = json$meta_data$cost_type
-customers = json$customers
-depots = json$depots
+clientes = json$customers
+depositos = json$depots
 
-customerDistances = matrix(nrow=n_customers, ncol = n_customers)
-customerDemand = matrix(ncol=n_customers, nrow=1)
-depotsDistances = matrix(nrow = n_customers, ncol = n_depots)
+customerDistances = matrix(nrow=nr_clientes, ncol = nr_clientes)
+customerDemand = matrix(ncol=nr_clientes, nrow=1)
+depositosDistances = matrix(nrow = nr_clientes, ncol = nr_depositos)
+depositosStock = matrix(ncol=nr_depositos, nrow=1)
+#calculate clientes distances from c0 -> c49
 
-#calculate customers distances from c0 -> c49
+i = 1
+for(depot in depositos){
+  depositosStock[i] = depot$capacity
+  i = i+1
+}
 
 i=1
 j=1
 
-for (c1 in customers){
+for (c1 in clientes){
   x1 = c1$x;
   y1 = c1$y;
   customerDemand[i] = c1$demand;
-  for (c2 in customers){
+  for (c2 in clientes){
     x2 = c2$x;
     y2 = c2$y;
     d = sqrt((x1 - x2)^2 + (y1-y2)^2);
@@ -41,18 +47,19 @@ for (c1 in customers){
 i=1
 j=1
 
-for (c in customers){
+for (c in clientes){
   x1 = c$x;
   y1 = c$y;
-  for (d in depots){
+  for (d in depositos){
     x2 = d$x;
     y2 = d$y;
     distance = sqrt((x1 - x2)^2 + (y1-y2)^2);
-    depotsDistances[i,j] = distance;
+    depositosDistances[i,j] = distance;
     j = j+1;
   }
   j=1
   i = i+1;
 }
 
-
+remove(i,j, x1, x2, y1, y2, distance, c, c1, c2, d, depot)
+remove(clientes, depositos, json)
--- src/ga/tsp.R
+++ src/ga/tsp.R
@@ -1,15 +1,29 @@
 library(GA)
 
-nr_clientes = 50; #length(cli_Demand)
-nr_camioes = 5 #ncol(cli_Depots_Dist) # pior cenário há tantos quantos depositos. 
 cli_Dist = as.matrix(customerDistances);
 cli_Demand = as.matrix(customerDemand);
 cli_Depots_Dist = as.matrix(depotsDistances);
+nr_clientes = ncol(cli_Demand)
+nr_camioes = nr_clientes # pior cenário há tantos quantos depositos. 
+nr_depositos = ncol(depositosStock)
 
-#vehicle_cap
-#vehicle_cost 
-#cost_type
-#customers
+# Função para calcular o custo por kms percorrido num dado percurso
+custo_viagem <- function(cli_Depots_Dist, cli_Dist, percurso){
+  custo = 0
+  
+  # Inicio: custo do deposito i ao 1º cliente visitado
+  custo = custo + cli_Depots_Dist[1, i]
+  
+  # Rota: Calcular o custo por km percorrido na rota de 1 camião
+  percurso = c(percurso, percurso[1])
+  rota = embed(percurso, 2)[,2:1]
+  custo = custo + sum(cli_Dist[rota])
+  
+  # Regresso: custo de regressar do último cliente ao deposito i
+  custo = custo + cli_Depots_Dist[nrow(cli_Depots_Dist), i]
+  
+  return(custo)
+}
 
 #Function to calculate tour length 
 tourLength <- function(tour, cli_Dist, cli_Demand, cli_Depots_Dist) {
@@ -23,54 +37,69 @@ tourLength <- function(tour, cli_Dist, cli_Demand, cli_Depots_Dist) {
   # array para saber se a solução visita todos os clientes
   cli_visitados = matrix(0, ncol = nr_clientes, nrow = 1)
   
-  for (i in c(1:nrow(percursos))){
-    # Se não está tudo a zero, é pq há percurso no camião i 
-    if(sum(percursos[i,]) != 0){
-      percurso = percursos[i,]
-      
-      # Marcar quais os clientes visitados 
-      for (p in c(1:ncol(percursos))){
-        if(percurso[p]!= 0){
-          cli_visitados[p] = 1; 
-        }
-      }
-      
-      # custo base de deslocar um camião 
-      custo = custo + vehicle_cost
-      
-      # Inicio: custo do deposito i ao 1º cliente visitado
-      custo = custo + cli_Depots_Dist[i,1]
-        
-      # Rota: Calcular o custo por km percorrido na rota de 1 camião
-      percurso = c(percurso, percurso[1])
-      rota = embed(percurso, 2)[,2:1]
-      custo = custo + sum(cli_Dist[rota])
-      
-      # Regresso: custo de regressar do último cliente ao deposito i
-      custo = custo + cli_Depots_Dist[i,length((cli_Depots_Dist))]
-    }
-  }
+  # vetor com a procura de cada client. No final, a soma deve ser zero (toda a procura satisfeita)
+  procura = cli_Demand
   
-  if(sum(cli_visitados) != nr_clientes){
-    return(1000000) 
-  }else return(-custo)
+  for (i in c(1:nrow(percursos))){
+    # Ficar apenas com os valores do cromossoma entre [1: nr_cliente ]
+    percurso = intersect(percursos[i,], c(1:nr_clientes))
+    
+    carga = vehicle_cap # Carga inicial do camião no inicio do percurso
+    stock = depositosStock[i] # 
+    
+    # Marcar quais os clientes visitados 
+    for (p in c(1:length(percurso))){
+      # O camião faz um percurso por clientes, sem carga para os satisfazer até ao final, 
+      # mesmo reabastecendo no stock do deposito 
+      if((carga==0)&& (stock==0) && (p!=length(percurso))){  return(0)  }
+      # cliente já satisfeito por outros camiões. Não se justifica vir até ele se a sua procura está a 0. 
+      if(procura[p] == 0 ){ return(0)}
+      
+      if(carga >= procura[p]){
+        entregar = procura[p] # entregar tudo de uma vez, pq ainbda há carga no camião
+      }else{
+        entregar = carga # entregar só o que temos 
+      }
+      carga = carga - entregar; # reduzir capacidade do camião
+      procura[p] = procura[p] - entregar # reduzir a procura do cliente 
+      cli_visitados[p] = 1; 
+    }
+    
+    # custo base de deslocar um camião 
+    custo = custo + vehicle_cost
+  
+    # Calcular o custo de uma viagem 
+    #c_viagem <- custo_viagem(cli_Depots_Dist, cli_Dist, percurso)
+    #custo = custo + c_viagem
+  }
+
+  # Se não se visitaram todos os clientes OU a procura não foi satisfeita, a solução nao é valida
+  if((sum(cli_visitados)!=nr_clientes) || (sum(procura[p])!=0)){  return(0) }
+  else{ return(-custo) }
+  
 }
 
 #Firness function to be maximized
-tspFitness <- function(tour, ...) 1/tourLength(tour, ...)
-cli_Depots_Dist[3,]
+tspFitness <- function(tour, ...) tourLength(tour, ...)
+
 GA <- ga(type = "permutation", fitness = tspFitness,
          cli_Dist=cli_Dist, cli_Demand=cli_Demand, cli_Depots_Dist=cli_Depots_Dist,
-         nBits = nr_camioes*nr_clientes,
-         min = 1, max = nr_camioes*nr_clientes, popSize = 50, maxiter = 150,
+         nBits = nr_*nr_clientes,
+         min = 1, max = nr_camioes*nr_clientes, popSize = 50, maxiter = 500,
          run = 500, pmutation = 0.2)
 
 summary(GA)
 
+custo = 0
 tour = GA@solution[1,]
-tour <- c(tour, tour[1])
-route <- embed(tour, 2)[,2:1]
-cat("custo = ", sum(cli_Dist[route]))
+tour = matrix(data=tour,nrow=nr_camioes, ncol =nr_clientes)
+for(i in c(1:nrow(tour))){
+  percurso = intersect(tour[i,], c(1:nr_clientes))
+  percurso = c(percurso, percurso[1])
+  percurso <- embed(percurso, 2)[,2:1]
+  custo = custo + sum(custo_viagem(cli_Depots_Dist, cli_Dist, percurso))
+  }
+cat("custo = ", custo )
 
 #Visualization 
 

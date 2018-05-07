971d654eea3096a67a8d6a4e4358c8cf3b9b956c
Rogério Moreira <r@rgllm.com>
2018-05-07 12:05:11 +0100

    cleaning the house 🛀

--- 04/CN/TP3/src/src/parser/PsoTools.py
+++ 04/CN/TP3/src/src/parser/PsoTools.py
@@ -1,32 +1,3 @@
-#!/usr/bin/python
-#######################################################################
-# This is a simple script that formats input data to JSON file
-########################################################################
-# The structure of the files is as follows:
-#
-# number of customers
-# number of available depots
-#
-# coordinates for the depots (x and y)
-#
-# coordinates for the customers
-#
-# vehicle capacity
-#
-# depot capacities (for Tuzun instances, each one is equal to the total demand as there is no capacity on the depots)
-#
-# customers demands
-#
-# opening costs for the depots
-#
-# opening cost of a route (cost of a vehicle)
-#
-# 0 or 1 (0 means that the costs are integer - 1 that costs are real)
-#
-# To calculate the matrix distance (or the cost to link any 2 points A and B in the graph), we use the mathematical formula:
-# sqrt( (xA-xB)^2 + (yA-yB)^2 )
-# The results are stored in a float variable (in C language) if the costs are real (code 1)
-# The result is multiplied by 100 and truncked to be stored in an integer variable if the costs are interger (code 0).
 import itertools
 import json
 import matplotlib.pyplot as plt

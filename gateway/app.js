7c7048fe1600a365ff3be466896be280862b1ffe
munybt <a76507@alunos.uminho.pt>
2018-06-11 18:45:20 +0100

    uso do package 'cors' no microsserviço de users

--- gateway/app.js
+++ gateway/app.js
@@ -4,19 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
 var cryptoRouter = require('./routes/crypto');
 var usersRouter = require('./routes/users');
 
-
 var app = express();
-app.use(allowCrossDomain)
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- users/app.js
+++ users/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var mongoose = require('mongoose')
 var User = require('./models/User')
 var session = require('express-session');
+var cors = require('cors')
 // Autenticação FB 
 var passport = require('passport'), 
 	FacebookStrategy = require('passport-facebook').Strategy;
@@ -57,6 +58,7 @@ var indexRouter = require('./routes/index');
 var testsRouter = require('./routes/tests');
 
 var app = express();
+app.use(cors())
 app.use(session({
   secret: "trding2018",
   resave: true,

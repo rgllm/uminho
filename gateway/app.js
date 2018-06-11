444ce04c15d48f9e1fbe1b847f8fbbcd706d7e73
munybt <a76507@alunos.uminho.pt>
2018-06-11 18:43:15 +0100

    asd

--- gateway/app.js
+++ gateway/app.js
@@ -3,14 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
 
 var cryptoRouter = require('./routes/crypto');
 var usersRouter = require('./routes/users');
 
 
 var app = express();
-app.use(cors())
+app.use(allowCrossDomain)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');

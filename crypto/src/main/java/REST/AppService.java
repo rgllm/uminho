3ee964dea195cdfa4f9d59f6878899478401563a
João Reis <johndivision@live.com.pt>
2018-06-13 23:00:02 +0100

    CORS changed

--- crypto/src/main/java/REST/AppService.java
+++ crypto/src/main/java/REST/AppService.java
@@ -13,7 +13,6 @@ import javax.servlet.FilterRegistration;
 import java.util.EnumSet;
 
 
-
 public class AppService extends Application<AppConfig> {
 
     public AppService() {
@@ -30,20 +29,31 @@ public class AppService extends Application<AppConfig> {
         return "Currencies server";
     }
 
+
+    public static final String CORSFILTER = "CORS";
+    private static final String CORS_SUPPORTED_METHODS = "cors.supportedMethods";
+    private static final String CORS_SUPPORTS_CREDENTIALS = "cors.supportsCredentials";
+    private static final String CORS_EXPOSED_HEADERS = "cors.exposedHeaders";
+    private static final String CORS_SUPPORTED_HEADERS = "cors.supportedHeaders";
+    private static final String CORS_ALLOW_ORIGIN = "cors.allowOrigin";
+
+
     @Override
     public void run(AppConfig appConfig, Environment environment) throws Exception {
+
+
         final FilterRegistration.Dynamic cors =
                 environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
         // Configure CORS parameters
         cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedHeaders", "Cache-Control,If-Modified-Since,Pragma,Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
         cors.setInitParameter("allowedMethods", "GET");
 
         // Add URL mapping
         cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
-	final Currencies currencies = new Currencies();
+        final Currencies currencies = new Currencies();
         final Autocomplete ac = new Autocomplete();
         final Resources.Currency c = new Resources.Currency();
         environment.jersey().register(currencies);

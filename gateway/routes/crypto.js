669d9caecb88b7a8b0b3e427c65a7cd949888087
munybt <a76507@alunos.uminho.pt>
2018-06-13 22:51:37 +0100

     

--- gateway/routes/crypto.js
+++ gateway/routes/crypto.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request')
+
 
 var crypto_service_URL = "http://167.99.193.161"
 /* GET home page. */
@@ -8,15 +10,19 @@ router.get('/cryptocurrency/:id', function(req, res, next) {
 });
 
 router.get('/cryptocurrencies/', function(req, res, next) {
+  /*
   var query = "?"
   if(req.query.page){
     query+= "page=" + req.query.page
     if(req.query.perPage){
       query+= "&perPage=" + req.query.perPage
     }
-  }
-  
-  res.redirect(crypto_service_URL + `/cryptocurrencies${query!="?" ? query : "" }`)
+  }*/
+  request({
+    url: req.query.url,
+    method: req.query.method
+  }).pipe(res);
+  ///res.redirect(crypto_service_URL + `/cryptocurrencies${query!="?" ? query : "" }`)
 });
 
 router.get('/autocomplete/', function(req, res, next) {

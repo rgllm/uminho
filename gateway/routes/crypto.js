348565222365550429d008507bf8f87ea78e72bc
munybt <a76507@alunos.uminho.pt>
2018-06-13 23:11:41 +0100

     

--- gateway/routes/crypto.js
+++ gateway/routes/crypto.js
@@ -6,25 +6,18 @@ var request = require('request')
 var crypto_service_URL = "http://167.99.193.161"
 /* GET home page. */
 router.get('/cryptocurrency/:id', function(req, res, next) {
-  res.redirect(crypto_service_URL + "/cryptocurrency" + "/" + req.params.id)
+  var url = crypto_service_URL + req.url
+  req.pipe(request(url)).pipe(res)
 });
 
 router.get('/cryptocurrencies/', function(req, res, next) {
-  /*
-  var query = "?"
-  if(req.query.page){
-    query+= "page=" + req.query.page
-    if(req.query.perPage){
-      query+= "&perPage=" + req.query.perPage
-    }
-  }*/
   var url = crypto_service_URL + req.url
   req.pipe(request(url)).pipe(res)
-  ///res.redirect(crypto_service_URL + `/cryptocurrencies${query!="?" ? query : "" }`)
 });
 
 router.get('/autocomplete/', function(req, res, next) {
-  res.redirect(crypto_service_URL + "/autocomplete" + `?searchQuery=${req.query.searchQuery}`  )
+  var url = crypto_service_URL + req.url
+  req.pipe(request(url)).pipe(res)
 });
 
 module.exports = router;
